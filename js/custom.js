f035876a48f9a61dd6ecc8fe293aa420dac1e3f3
Taras Borovets <legioner.ukr@gmail.com>
2016-10-18 06:58:54 +0000

    ff

--- js/custom.js
+++ js/custom.js
@@ -15,11 +15,11 @@ $(document).ready(function() {
 
     $("#owl-example").owlCarousel({
 
-        autoPlay: 4000,
+        autoPlay: 5000,
         stopOnHover: true,
         navigation: true,
-        paginationSpeed: 3000,
-        goToFirstSpeed: 2000,
+        paginationSpeed: 2000,
+        goToFirstSpeed: 3000,
         singleItem: true,
         navigationText: ["previous", "next"],
         responsive: true,
@@ -34,4 +34,50 @@ $(document).ready(function() {
             scrollTop: top
         }, 1500);
     });
+    console.log( $("#form-submit"));
+     $("#form-submit").submit(function eventHandler(event){
+        event.preventDefault();
+        // console.log(event,$(event.target).find("input[type='email']").val());
+        
+        var email = $(event.target).find("input[name='email']").val();
+        var surname = $(event.target).find("input[name='surname']").val();
+        var name = $(event.target).find("input[name='name']").val();
+        var comment = $(event.target).find("textarea[name='comment']").val();
+        console.log(email,name,surname,comment);
+        
+        $.ajax({
+            url: "https://formspree.io/legioner.ukr@gmail.com", 
+            method: "POST",
+            data: {
+                email: email,
+                surname: surname,
+                name: name,
+                comment: comment
+            },
+            dataType: "json",
+            success : function(response){
+               
+                alert("success:" + response.success);
+            },
+            error: function(response){
+                alert("error: " + response.responseJSON.error);
+            }
+        });
+    });
+         
 });
+ var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    
+    // If we need pagination
+    pagination: '.swiper-pagination',
+    
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    
+    // And if we need scrollbar
+    scrollbar: '.swiper-scrollbar',
+  }) 
